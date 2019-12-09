b4b9aa246cb621296aa81eb46f44236a7c722a3c
Dmitry <crismsfiva@gmail.com>
2019-12-10 01:11:00 +0300

    refactor brain-games.js and index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import printGreeting from '..';
+import { printGreeting, sayHelloToUser } from '..';
 
-console.log('Welcome to the Brain Games!');
 printGreeting();
+sayHelloToUser();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const printGreeting = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+const sayHelloToUser = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
 };
+
+export { printGreeting, sayHelloToUser };
