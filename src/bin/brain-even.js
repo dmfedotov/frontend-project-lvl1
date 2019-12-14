e856ac6dba501ea7ed568d9b0764a95bb86298b0
Dmitry <crismsfiva@gmail.com>
2019-12-14 13:53:21 +0300

    change brain-even game according to new game logic

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import { startGame } from '..';
-import { RULES, generateQuestion, calculateAnswer } from '../games/brain-even';
+import { RULES, generateQuestion } from '../games/brain-even';
 
-startGame(RULES, generateQuestion, calculateAnswer);
+startGame(RULES, generateQuestion);
--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,12 +1,14 @@
+import { cons } from '@hexlet/pairs';
+
 const RULES = 'Answer "yes" if the number is even, otherwise answer "no".\n';
 
 const getRandomNum = (start, end) => Math.round(Math.random() * (end - start) + start);
-const generateQuestion = () => getRandomNum(1, 100);
 const calculateAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const generateQuestion = () => {
+  const question = getRandomNum(1, 100);
+  const answer = calculateAnswer(question);
 
-export {
-  RULES,
-  generateQuestion,
-  calculateAnswer,
-  getRandomNum,
+  return cons(question, answer);
 };
+
+export { RULES, generateQuestion, getRandomNum };
