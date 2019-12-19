0b0d1c830251495dcb56a4800f55cd2ae4633432
Dmitry <crismsfiva@gmail.com>
2019-12-19 18:29:46 +0300

    refactor brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,10 +5,10 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".\n';
 const minNumber = 1;
 const maxNumber = 100;
 
-const calculateAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => num % 2 === 0;
 const generateQuestion = () => {
   const question = getRandomNum(minNumber, maxNumber);
-  const answer = calculateAnswer(question);
+  const answer = isEven(question) ? 'yes' : 'no';
 
   return cons(question, answer);
 };
