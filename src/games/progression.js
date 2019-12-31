304bb34a07d154f175c9c1be60c9ed4d2143f5c3
Dmitry <crismsfiva@gmail.com>
2019-12-31 14:44:28 +0300

    refactor progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,21 +6,25 @@ const description = 'What number is missing in the progression?';
 const progressionLength = 10;
 const minNumber = 1;
 
-const generateQuestion = (initNum, step, missed, counter, acc) => {
-  if (counter > progressionLength) {
-    return acc.trim();
-  }
-  if (counter === missed) {
-    return generateQuestion(initNum, step, missed, counter + 1, `${acc} ..`);
-  }
-  return generateQuestion(initNum, step, missed, counter + 1, `${acc} ${initNum + step * (counter - 1)}`);
+const generateQuestion = (initNum, step, missed) => {
+  const iter = (counter, acc) => {
+    if (counter > progressionLength) {
+      return acc.trim();
+    }
+    if (counter === missed) {
+      return iter(counter + 1, `${acc} ..`);
+    }
+    return iter(counter + 1, `${acc} ${initNum + step * (counter - 1)}`);
+  };
+
+  return iter(1, '');
 };
 
 const makeQuestionAnswer = () => {
   const missingPosition = getRandomNum(minNumber, progressionLength);
   const progressionStep = getRandomNum(minNumber, progressionLength);
   const initNumber = getRandomNum(minNumber, progressionLength);
-  const question = generateQuestion(initNumber, progressionStep, missingPosition, 1, '');
+  const question = generateQuestion(initNumber, progressionStep, missingPosition);
   const answer = String(initNumber + progressionStep * (missingPosition - 1));
 
   return cons(question, answer);
